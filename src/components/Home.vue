2a1de69e4822d30d87a4322591496b212e36b208
Nature_Day <100494782@qq.com>
2018-08-30 17:27:10 +0800

    init project

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        登录
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    components: {
+    }
+}
+</script>
+
+<style>
+</style>
